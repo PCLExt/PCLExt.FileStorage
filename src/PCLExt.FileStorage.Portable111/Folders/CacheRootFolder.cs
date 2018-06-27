d221e2f26dd64f9d822d894a0c9c60389cb432d5
aragas <micvitalij@yandex.ru>
2018-06-27 21:50:20 +0300

    Added folders from https://github.com/picolyl/XamarinPlugins.PCLSpecialFolder
    Updated NUnit packages
    Updated MSTest packages
    Renamed PCLExt.FileStorage.Core to PCLExt.FileStorage.Standard
    Added tests for new folders
    Updated version

---
+++ src/PCLExt.FileStorage.Portable111/Folders/CacheRootFolder.cs
@@ -0,0 +1,32 @@
+﻿using PCLExt.FileStorage.Extensions;
+using System.Linq;
+
+namespace PCLExt.FileStorage.Folders
+{
+    /// <summary>
+    /// A <see cref="BaseFolder"/> where the app is running.
+    /// </summary>
+    public class CacheRootFolder : BaseFolder
+    {
+        /// <summary>
+        /// Returns the <see cref="BaseFolder"/> where the app is running.
+        /// </summary>
+#if !PORTABLE
+        public CacheRootFolder() : base(GetCacheFolder()) { }
+        private static IFolder GetCacheFolder()
+        {
+#if ANDROID
+            return new DefaultFolderImplementation(Android.App.Application.Context.CacheDir.AbsolutePath);
+#elif __IOS__
+            return new DefaultFolderImplementation(Foundation.NSSearchPath.GetDirectories(Foundation.NSSearchPathDirectory.CachesDirectory, Foundation.NSSearchPathDomain.User).FirstOrDefault());
+#elif DESKTOP || __MACOS__ || NETSTANDARD2_0
+            return new DefaultFolderImplementation(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData)).CreateFolder("Cache", CreationCollisionOption.OpenIfExists);
+#elif WINDOWS_UWP
+            return new UWP.StorageFolderImplementation(Windows.Storage.ApplicationData.Current.LocalCacheFolder.Path);
+#endif
+        }
+#else
+        public CacheRootFolder() : base(null) => throw Exceptions.ExceptionsHelper.NotImplementedInReferenceAssembly();
+#endif
+    }
+}
---
+++ src/PCLExt.FileStorage.Portable111/Folders/VideosRootFolder.cs
@@ -0,0 +1,32 @@
+﻿using PCLExt.FileStorage.Extensions;
+using System.Linq;
+
+namespace PCLExt.FileStorage.Folders
+{
+    /// <summary>
+    /// A <see cref="BaseFolder"/> where the app is running.
+    /// </summary>
+    public class VideosRootFolder : BaseFolder
+    {
+        /// <summary>
+        /// Returns the <see cref="BaseFolder"/> where the app is running.
+        /// </summary>
+#if !PORTABLE
+        public VideosRootFolder() : base(GetVideosFolder()) { }
+        private static IFolder GetVideosFolder()
+        {
+#if ANDROID
+            return new DefaultFolderImplementation(Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryMovies).AbsolutePath);
+#elif __IOS__
+            return new DefaultFolderImplementation(Foundation.NSSearchPath.GetDirectories(Foundation.NSSearchPathDirectory.MoviesDirectory, Foundation.NSSearchPathDomain.User).FirstOrDefault());
+#elif DESKTOP || __MACOS__ || NETSTANDARD2_0
+            return new DefaultFolderImplementation(System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyVideos)).CreateFolder("Cache", CreationCollisionOption.OpenIfExists);
+#elif WINDOWS_UWP
+            return null;
+#endif
+        }
+#else
+        public VideosRootFolder() : base(null) => throw Exceptions.ExceptionsHelper.NotImplementedInReferenceAssembly();
+#endif
+    }
+}
