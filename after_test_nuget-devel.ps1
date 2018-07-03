926dd5c2c28fb70e88120699039bed907a967ad1
aragas <micvitalij@yandex.ru>
2018-07-03 20:33:51 +0300

    nuget fix

--- after_test_nuget-devel.ps1
+++ after_test_nuget-devel.ps1
@@ -2,6 +2,6 @@ if($env:CONFIGURATION -eq "Release") # is not a pull request
 {
 	cd $env:APPVEYOR_BUILD_FOLDER
 	cd common
-	nuget pack PCLExt.FileStorage.nuspec -Version $env:APPVEYOR_BUILD_VERSION
+	nuget pack PCLExt.FileStorage.-develnuspec -Version $env:APPVEYOR_BUILD_VERSION
 	Push-AppveyorArtifact "PCLExt.FileStorage.*.nupkg" -FileName nuget
 }
