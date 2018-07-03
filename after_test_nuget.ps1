bc36719e34f75ad5c874c1c7499112f9b8d6ad9a
aragas <micvitalij@yandex.ru>
2018-07-03 20:53:40 +0300

    using Get-ChildItem to get the exact nuget filename

--- after_test_nuget.ps1
+++ after_test_nuget.ps1
@@ -3,5 +3,6 @@ if($env:CONFIGURATION -eq "Release") # is not a pull request
 	cd $env:APPVEYOR_BUILD_FOLDER
 	cd common
 	nuget pack PCLExt.FileStorage.nuspec -Version $env:APPVEYOR_BUILD_VERSION
-	Push-AppveyorArtifact "PCLExt.FileStorage.*.nupkg" -FileName nuget
+	$nupkg = (Get-ChildItem PCLExt.FileStorage*.nupkg)[0];
+	Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "PCLExt.FileStorage.nupkg";
 }
