95f7b2fb34260923df59e9e881d4771de8e3e2b1
aragas <micvitalij@yandex.ru>
2018-07-05 00:12:40 +0300

    fix building

---
+++ build.ps1
@@ -0,0 +1,15 @@
+if($isWindows)
+{
+	msbuild PCLExt.FileStorage.sln /verbosity:m
+}
+if($isLinux)
+{
+	#dotnet build src/PCLExt.FileStorage.Abstractions
+	#dotnet build src/PCLExt.FileStorage.Standard
+	#FrameworkPathOverride=$MONO_BASE_PATH/4.5-api/ dotnet build src/PCLExt.FileStorage.NetFX
+	#dotnet build test/PCLExt.FileStorage.Core.Test
+	#FrameworkPathOverride=$MONO_BASE_PATH/4.5-api/ dotnet build test/PCLExt.FileStorage.NetFX.Test
+	dotnet test test/PCLExt.FileStorage.Core.Test/PCLExt.FileStorage.Core.Test.csproj
+	nuget install NUnit.ConsoleRunner -Version 3.8.0 -OutputDirectory packages
+	mono packages/NUnit.ConsoleRunner.3.8.0/tools/nunit3-console.exe test/PCLExt.FileStorage.NetFX.Test/bin/Debug/PCLExt.FileStorage.NetFX.Test.dll
+}
