608f33974abff0afa39fe906ca7368d98cd0b439
aragas <micvitalij@yandex.ru>
2018-07-05 03:25:21 +0300

    build fix

--- build.ps1
+++ build.ps1
@@ -6,10 +6,12 @@ if($isLinux)
 {
 	msbuild src/PCLExt.FileStorage.Abstractions
 	msbuild src/PCLExt.FileStorage.Standard
+	msbuild src/PCLExt.FileStorage.Portable111
+	msbuild src/PCLExt.FileStorage.Portable259
 	msbuild src/PCLExt.FileStorage.NetFX
 	msbuild test/PCLExt.FileStorage.NetFX.Test
-	nuget install NUnit.ConsoleRunner -Version 3.8.0 -OutputDirectory packages
-	mono packages/NUnit.ConsoleRunner.3.8.0/tools/nunit3-console.exe test/PCLExt.FileStorage.NetFX.Test/bin/Debug/PCLExt.FileStorage.NetFX.Test.dll
+	nuget install NUnit.ConsoleRunner -Version 3.9.0 -OutputDirectory packages
+	mono packages/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe test/PCLExt.FileStorage.NetFX.Test/bin/Debug/PCLExt.FileStorage.NetFX.Test.dll
 
 	dotnet test test/PCLExt.FileStorage.Core.Test
 }
