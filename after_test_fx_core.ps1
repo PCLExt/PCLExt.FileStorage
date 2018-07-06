9c7af32c4a13df919437b5afb67d592c73d83d99
aragas <micvitalij@yandex.ru>
2018-07-07 02:51:08 +0300

    NETFX should now test Release configuration too

--- after_test_fx_core.ps1
+++ after_test_fx_core.ps1
@@ -14,7 +14,7 @@ if($isWindows) #no need to do the coverage on Ubuntu
 	CD $env:APPVEYOR_BUILD_FOLDER
 	nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 	nuget install NUnit.ConsoleRunner -Version 3.8.0 -OutputDirectory tools
-	.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -filter:"+[PCLExt.*]* -[PCLExt.FileStorage.NetFX.Test]*" -register:user -target:".\tools\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" -targetargs:"/domain:single test/PCLExt.FileStorage.NetFX.Test/bin/Debug/PCLExt.FileStorage.NetFX.Test.dll" -output:coverage_netfx.xml
+	.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -filter:"+[PCLExt.*]* -[PCLExt.FileStorage.NetFX.Test]*" -register:user -target:".\tools\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" -targetargs:"/domain:single test/PCLExt.FileStorage.NetFX.Test/bin/$env:CONFIGURATION/PCLExt.FileStorage.NetFX.Test.dll" -output:coverage_netfx.xml
 	csmacnz.coveralls --opencover -i coverage_netfx.xml --repoToken $env:COVERALLS_REPO_TOKEN
 	codecov -f coverage_netfx.xml
 }
