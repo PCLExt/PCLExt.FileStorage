93ce473e28ee6a318d37f185005e74083a3f31c8
Vitalij Mikhailov <micvitalij@yandex.ru>
2018-07-08 01:51:31 +0300

    Update test_script.ps1

--- test_script.ps1
+++ test_script.ps1
@@ -7,7 +7,7 @@ if($isLinux)
 	wget "https://github.com/nunit/nunit-console/releases/download/3.8/NUnit.Console-3.8.0.zip"
 	unzip "NUnit.Console-3.8.0.zip" -d "nunit"
 	
-	mono nunit/nunit3-console ./test/PCLExt.FileStorage.NetFX.Test/bin/$env:CONFIGURATION/PCLExt.FileStorage.NetFX.Test.dll --result="fx-result.xml"
+	mono nunit/nunit3-console.exe ./test/PCLExt.FileStorage.NetFX.Test/bin/$env:CONFIGURATION/PCLExt.FileStorage.NetFX.Test.dll --result="fx-result.xml"
 }
 
 dotnet vstest ./test/PCLExt.FileStorage.Core.Test/bin/$env:CONFIGURATION/netcoreapp2.0/PCLExt.FileStorage.Core.Test.dll --logger:"trx;LogFileName=../core-result.trx"
