7c8ecc2f0883cab28289e60ec7fb92016326b507
aragas <micvitalij@yandex.ru>
2018-07-08 01:30:05 +0300

    added nunit in ubuntu
    fixed configuration path

--- test_script.ps1
+++ test_script.ps1
@@ -1,3 +1,13 @@
-nunit3-console ./test/PCLExt.FileStorage.NetFX.Test/bin/Release/PCLExt.FileStorage.NetFX.Test.dll --result="fx-result.xml"
+if($isWindows)
+{
+	nunit3-console ./test/PCLExt.FileStorage.NetFX.Test/bin/$env:CONFIGURATION/PCLExt.FileStorage.NetFX.Test.dll --result="fx-result.xml"
+}
+if($isLinux)
+{
+	wget "https://github.com/nunit/nunit-console/releases/download/3.8/NUnit.Console-3.8.0.zip"
+	unzip "NUnit.Console-3.8.0.zip" "nunit"
+	
+	mono nunit/nunit3-console ./test/PCLExt.FileStorage.NetFX.Test/bin/$env:CONFIGURATION/PCLExt.FileStorage.NetFX.Test.dll --result="fx-result.xml"
+}
 
-dotnet vstest ./test/PCLExt.FileStorage.Core.Test/bin/Release/netcoreapp2.0/PCLExt.FileStorage.Core.Test.dll --logger:"trx;LogFileName=../core-result.trx"
+dotnet vstest ./test/PCLExt.FileStorage.Core.Test/bin/$env:CONFIGURATION/netcoreapp2.0/PCLExt.FileStorage.Core.Test.dll --logger:"trx;LogFileName=../core-result.trx"
