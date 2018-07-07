e242eb66e2b18cb5695bb5efff0ad95f64207cfd
aragas <micvitalij@yandex.ru>
2018-07-08 01:00:58 +0300

    fixed parameters

--- test_script.ps1
+++ test_script.ps1
@@ -1,3 +1,3 @@
-nunit3-console ./test/PCLExt.FileStorage.NetFX.Test/bin/Release/PCLExt.FileStorage.NetFX.Test.dll --result=fx-result.xml;format=AppVeyor
+nunit3-console ./test/PCLExt.FileStorage.NetFX.Test/bin/Release/PCLExt.FileStorage.NetFX.Test.dll --result="fx-result.xml"
 
-dotnet vstest ./test/PCLExt.FileStorage.Core.Test/bin/Release/netcoreapp2.0/PCLExt.FileStorage.Core.Test.dll /logger:trx;LogFileName=core-results.trx
+dotnet vstest ./test/PCLExt.FileStorage.Core.Test/bin/Release/netcoreapp2.0/PCLExt.FileStorage.Core.Test.dll --logger:"trx;LogFileName=../core-result.trx"
