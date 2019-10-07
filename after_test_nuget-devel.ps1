6cc6afed34da7db22e5043833b1bb47a1bd40b3a
aragas <personal@aragas.org>
2019-10-07 06:52:14 +0300

    Added GPE

--- after_test_nuget-devel.ps1
+++ after_test_nuget-devel.ps1
@@ -3,6 +3,10 @@ if($env:CONFIGURATION -eq "Release" -and $isWindows) # is not a pull request
 	cd $env:APPVEYOR_BUILD_FOLDER
 	cd common
 	nuget pack PCLExt.FileStorage-devel.nuspec -Version $env:APPVEYOR_BUILD_VERSION
+	
 	$nupkg = (Get-ChildItem PCLExt.FileStorage-devel*.nupkg)[0];
 	Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "PCLExt.FileStorage-devel.nupkg";
+	
+	nuget source Add -Name "PCLExt" -Source "https://nuget.pkg.github.com/PCLExt/index.json" -UserName PCLExt -Password $env:GITHUB_NUGET
+	nuget push $nupkg.FullName -Source "PCLExt"
 }
--- after_test_nuget.ps1
+++ after_test_nuget.ps1
@@ -3,6 +3,10 @@ if($env:CONFIGURATION -eq "Release" -and $env:APPVEYOR_REPO_TAG -eq "true" -and 
 	cd $env:APPVEYOR_BUILD_FOLDER
 	cd common
 	nuget pack PCLExt.FileStorage.nuspec -Version $env:APPVEYOR_BUILD_VERSION
+	
 	$nupkg = (Get-ChildItem PCLExt.FileStorage*.nupkg)[0];
 	Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "PCLExt.FileStorage.nupkg";
+	
+	nuget source Add -Name "PCLExt" -Source "https://nuget.pkg.github.com/PCLExt/index.json" -UserName PCLExt -Password $env:GITHUB_NUGET
+	nuget push $nupkg.FullName -Source "PCLExt"
 }
