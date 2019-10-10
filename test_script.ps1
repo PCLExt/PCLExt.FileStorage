01c1d900709ef163b9919686012fb27f25331b33
Vitaly Mikhailov <personal@aragas.org>
2019-10-10 22:35:09 +0300

    fix codecov execution

--- test_script.ps1
+++ test_script.ps1
@@ -32,16 +32,15 @@ if($isLinux)
 	(New-Object System.Net.WebClient).DownloadFile("https://github.com/nunit/nunit-console/releases/download/v3.10/NUnit.Console-3.10.0.zip", "$(Get-Location)/NUnit.Console.zip")
 	Expand-Archive .\NUnit.Console.zip
 	
-	#mono NUnit.Console/bin/net35/nunit3-console.exe ./test/PCLExt.FileStorage.NetFX.Test/bin/$env:CONFIGURATION/PCLExt.FileStorage.NetFX.Test.dll --result="fx-result.xml"
 	mono opencover/OpenCover.Console.exe -filter:"+[PCLExt.*]* -[PCLExt.FileStorage.NetFX.Test*]*" -register:user -target:"NUnit.Console/bin/net35/nunit3-console.exe" -targetargs:"/domain:single test/PCLExt.FileStorage.NetFX.Test/bin/$env:CONFIGURATION/PCLExt.FileStorage.NetFX.Test.dll" -output:coverage_netfx.xml
-	mono Codecov/codecov.exe -f coverage_netfx.xml
+	./Codecov/codecov -f coverage_netfx.xml
 
 	dotnet publish test/PCLExt.FileStorage.Core.Test/PCLExt.FileStorage.Core.Test.csproj -f netcoreapp3.0 --no-restore
 	dotnet vstest ./test/PCLExt.FileStorage.Core.Test/bin/$env:CONFIGURATION/netcoreapp3.0/PCLExt.FileStorage.Core.Test.dll --logger:"trx;LogFileName=../core-result.trx"
 	Get-ChildItem "test/PCLExt.FileStorage.Core.Test/TestResults" -Recurse -File -Filter "coverage.cobertura.xml" | Sort-Object -Property LastWriteTime | foreach { copy $_.FullName "test/PCLExt.FileStorage.Core.Test" }
-	mono Codecov\codecov.exe -f test/PCLExt.FileStorage.Core.Test/coverage.cobertura.xml
+	./Codecov/codecov -f test/PCLExt.FileStorage.Core.Test/coverage.cobertura.xml
 	
 	# manually upload test results
-	(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path 'fx-result.xml'))
-	(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", (Resolve-Path 'core-result.trx'))
+	#(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path 'fx-result.xml'))
+	#(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", (Resolve-Path 'core-result.trx'))
 }
