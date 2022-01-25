18e67c930339b267d3c34964a57177f829d93336
Vitaly Mikhailov <micvitalij@yandex.ru>
2022-01-25 14:21:07 +0300

    Update test_script.ps1

--- test_script.ps1
+++ test_script.ps1
@@ -17,8 +17,8 @@ if($isWindows)
 	#dotnet add test/PCLExt.FileStorage.Core.Test/PCLExt.FileStorage.Core.Test.csproj package coverlet.msbuild --no-restore
 	#&('dotnet') ('test', 'test/PCLExt.FileStorage.Core.Test/PCLExt.FileStorage.Core.Test.csproj', '/p:CollectCoverage=true', '/p:CoverletOutputFormat=opencover', '/p:Exclude=\"[NUnit*]*,[PCLExt.FileStorage.Core.Test*]*\"', '--logger:"trx;LogFileName=../core-result.trx"')
 	#dotnet add test/PCLExt.FileStorage.Core.Test/PCLExt.FileStorage.Core.Test.csproj package coverlet.collector --no-restore
-	dotnet publish test/PCLExt.FileStorage.Core.Test/PCLExt.FileStorage.Core.Test.csproj -f netcoreapp3.0 --no-restore
-	dotnet vstest test/PCLExt.FileStorage.Core.Test/bin/$env:CONFIGURATION/netcoreapp3.0/publish/PCLExt.FileStorage.Core.Test.dll --collect:"XPlat Code Coverage" --logger:"trx;LogFileName=../core-result.trx" --ResultsDirectory:"test/PCLExt.FileStorage.Core.Test/TestResults"
+	dotnet publish test/PCLExt.FileStorage.Core.Test/PCLExt.FileStorage.Core.Test.csproj -f netcoreapp2.0 --no-restore
+	dotnet vstest test/PCLExt.FileStorage.Core.Test/bin/$env:CONFIGURATION/netcoreapp2.0/publish/PCLExt.FileStorage.Core.Test.dll --collect:"XPlat Code Coverage" --logger:"trx;LogFileName=../core-result.trx" --ResultsDirectory:"test/PCLExt.FileStorage.Core.Test/TestResults"
 	Get-ChildItem "test/PCLExt.FileStorage.Core.Test/TestResults" -Recurse -File -Filter "coverage.cobertura.xml" | Sort-Object -Property LastWriteTime | foreach { copy $_.FullName "test/PCLExt.FileStorage.Core.Test" }
 	#csmacnz.coveralls --opencover -i test/PCLExt.FileStorage.Core.Test/coverage.cobertura.xml --repoToken $env:COVERALLS_REPO_TOKEN
 	Codecov\codecov.exe -f test/PCLExt.FileStorage.Core.Test/coverage.cobertura.xml
@@ -37,8 +37,8 @@ if($isLinux)
 	mono opencover/OpenCover.Console.exe -filter:"+[PCLExt.*]* -[PCLExt.FileStorage.NetFX.Test*]*" -register:user -target:"NUnit.Console/bin/net35/nunit3-console.exe" -targetargs:"/domain:single test/PCLExt.FileStorage.NetFX.Test/bin/$env:CONFIGURATION/PCLExt.FileStorage.NetFX.Test.dll" -output:coverage_netfx.xml
 	dotnet run Codecov/codecov -f coverage_netfx.xml
 
-	dotnet publish test/PCLExt.FileStorage.Core.Test/PCLExt.FileStorage.Core.Test.csproj -f netcoreapp3.0 --no-restore
-	dotnet vstest ./test/PCLExt.FileStorage.Core.Test/bin/$env:CONFIGURATION/netcoreapp3.0/PCLExt.FileStorage.Core.Test.dll --logger:"trx;LogFileName=../core-result.trx"
+	dotnet publish test/PCLExt.FileStorage.Core.Test/PCLExt.FileStorage.Core.Test.csproj -f netcoreapp2.0 --no-restore
+	dotnet vstest ./test/PCLExt.FileStorage.Core.Test/bin/$env:CONFIGURATION/netcoreapp2.0/PCLExt.FileStorage.Core.Test.dll --logger:"trx;LogFileName=../core-result.trx"
 	Get-ChildItem "test/PCLExt.FileStorage.Core.Test/TestResults" -Recurse -File -Filter "coverage.cobertura.xml" | Sort-Object -Property LastWriteTime | foreach { copy $_.FullName "test/PCLExt.FileStorage.Core.Test" }
 	dotnet run Codecov/codecov -f test/PCLExt.FileStorage.Core.Test/coverage.cobertura.xml
 	
